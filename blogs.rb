40f9a004319196bfb4193385e63a46975e5ff735
Byron Stuike <byron.stuike@gov.bc.ca>
2023-04-28 13:06:45 -0700

    Add ability to sort urls by type

--- blogs.rb
+++ blogs.rb
@@ -2,7 +2,8 @@
 $stdin.flush
 $stdout.flush
 $stdout.sync = true
-@raw = "["
+types = ["engage", "events", "forms", "vanity", "workingforyou"]
+blog, engage, events, forms, vanity, workingforyou = '"Misc": [', '"Engage": [', '"Events": [', '"Forms": [', '"Vanity": [', '"Workingforyou": ['
 
 e = File.read("blogs.txt")
 h = e.split(",")
@@ -15,12 +16,48 @@ end
 
 index = 0
 while index < h.length() do
-  @raw << '{"URL": "' << "#{h[index]}" << '"},'
-  index += 1
-  @raw << '{"ID": ' << "#{h[index]}" << '},'
-  index += 1
+  types.each do |line|
+    line.chomp!
+    if "#{h[index]}".include? "#{line}"
+      @variety = "#{line}"
+      break
+    else
+      @variety = "other"
+    end
+  end
+  case @variety
+  when "forms"
+    forms << '{"URL": "' << "#{h[index]}" << '"},'
+    index += 1
+    forms << '{"ID": ' << "#{h[index]}" << '},'
+    index += 1
+  when "engage"
+    engage << '{"URL": "' << "#{h[index]}" << '"},'
+    index += 1
+    engage << '{"ID": ' << "#{h[index]}" << '},'
+    index += 1
+  when "events"
+    events << '{"URL": "' << "#{h[index]}" << '"},'
+    index += 1
+    events << '{"ID": ' << "#{h[index]}" << '},'
+    index += 1
+  when "vanity"
+    vanity << '{"URL": "' << "#{h[index]}" << '"},'
+    index += 1
+    vanity << '{"ID": ' << "#{h[index]}" << '},'
+    index += 1
+  when "workingforyou"
+    workingforyou << '{"URL": "' << "#{h[index]}" << '"},'
+    index += 1
+    workingforyou << '{"ID": ' << "#{h[index]}" << '},'
+    index += 1
+  else
+    blog << '{"URL": "' << "#{h[index]}" << '"},'
+    index += 1
+    blog << '{"ID": ' << "#{h[index]}" << '},'
+    index += 1
+  end
 end
 
-@raw.chop!
-@raw << "]"
-scribble("blogs.json", @raw)
+compile = '{' << blog.chop! << '],' << engage.chop! << '],' << events.chop! << '],' << forms.chop! << '],' << vanity.chop! << '],' << workingforyou.chop! << ']}'
+scribble("blogs.json", compile)
